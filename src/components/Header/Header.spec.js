df8bb837f19582bab7c9d98e01b48d1747460ded
Luciana Blümlein <luziehtan@hotmail.com>
2021-03-12 11:20:38 +0100

    Add test for Header.js

---
+++ src/components/Header/Header.spec.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import Header from './Header'
+import imageFile from '../../images/eatnwatchlogo_new.png'
+
+describe('Header', () => {
+  it('renders a img and subtitle', () => {
+    render(<Header image={imageFile} subtitle={'This is a subtitle'} />)
+    expect(screen.getByText('This is a subtitle')).toBeInTheDocument()
+    expect(screen.getByRole('img')).toBeInTheDocument()
+  })
+})
