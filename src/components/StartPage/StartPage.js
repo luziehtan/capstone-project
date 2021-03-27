2da3c5dadb036badfaf04a0b02d0429028240acc
Luciana Blümlein <luziehtan@hotmail.com>
2021-03-27 17:48:11 +0100

    Change idea and fix problem - yay

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -10,7 +10,7 @@ export default function StartPage() {
   return (
     <StartingPage>
       <img src={Logo} alt="eatnwatchlogo" width="" height="80" />
-      <img src={LoadingGif} alt="eatnwatchloading" width="" height="80px" />
+      <Loading src={LoadingGif} alt="eatnwatchloading" width="" height="80px" />
       <StartButton as={Link} to="/play">
         Let's Play!
       </StartButton>
@@ -29,14 +29,26 @@ const StartingPage = styled.div`
   right: 0;
 `
 const StartButton = styled(Button)`
-  margin-top: 30px;
-  animation: cssAnimation 0s 2.7s forwards;
+  position: fixed;
+  top: 42%;
+  width: 70%;
+  animation: cssAnimation1 0s 3s forwards;
   visibility: hidden;
   border: 1px dashed var(--color-congopink);
 
-  @keyframes cssAnimation {
+  @keyframes cssAnimation1 {
     to {
       visibility: visible;
+    }
+  }
+`
+const Loading = styled.img`
+  animation: cssAnimation2 0s 3s forwards;
+  visibility: visible;
+
+  @keyframes cssAnimation2 {
+    to {
+      visibility: hidden;
     }
   }
 `
